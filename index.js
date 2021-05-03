39174919c10e22894998f4d0749fcc453af7a422
Dawid <baluszynskidawid@gmail.com>
2021-05-03 18:35:51 +0200

    refactor: add events to all buttons

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const buttonsContainer = document.querySelector('.js-swap');
+const buttonsContainer = document.querySelectorAll('.js-swap');
 const reviews = document.querySelectorAll('.review');
-
 const swapReview = (event) => {
   reviews.forEach(review => {
       review.classList.toggle('hidden');
   })
 }
 
-buttonsContainer.addEventListener('click', swapReview);
+buttonsContainer.forEach(btnContainer => btnContainer.addEventListener('click', swapReview));
+
